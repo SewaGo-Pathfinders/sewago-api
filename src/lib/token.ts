a36b27f3893222e79a61eb144d06902a2864101f
attaryu <mattarannaufal@gmail.com>
2024-01-11 23:56:15 +0700

    sign-in & sign-up

---
+++ src/lib/token.ts
@@ -0,0 +1,13 @@
+import type { JwtPayload } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
+
+export function createToken(payload: any, type: 'request' | 'refresh') {
+  if (type === 'request')
+    return sign(payload, <string>process.env.JSON_SECRET_KEY, { expiresIn: '10m' });
+
+  return sign(payload, <string>process.env.JSON_SECRET_KEY, { expiresIn: '30 days' });
+}
+
+export function verifyToken<T = string | JwtPayload>(token: string) {
+  return verify(token, <string>process.env.JSON_SECRET_KEY) as T;
+}
